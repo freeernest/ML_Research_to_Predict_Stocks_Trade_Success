05a85b94cb64f515ed1398362a3ee44b9d309bd9
erik.fe <erik.feigin@gigya-inc.com>
2022-08-21 10:04:59 +0300

    Working with trading view

--- src/ExcelReportFileConvertorForTDAmeritrade.py
+++ src/ExcelReportFileConvertorForTDAmeritrade.py
@@ -1,28 +1,32 @@
 import openpyxl
 from datetime import datetime
+import time as time_lib
 
 
-path = r'C:\Users\i506998\OneDrive - SAP SE\Documents\private\Master Degree\Final Project\My research\TD_Ameritrade\data.xlsx'
+path = r'C:\Users\i506998\OneDrive - SAP SE\Documents\private\Master Degree\Final Project\My research\TD_Ameritrade\data_mov_avg_two_lines.xlsx'
 wb_obj = openpyxl.load_workbook(path)
 sheet_obj = wb_obj.active
 
-results = open("results.csv", "a")
+results = open("results_new.csv", "a")
 results.truncate(0)
 
 header = 'MACD(),MACDdiff,MACD().Avg,RSI(),"ExpAverage(close, length = 9)","ExpAverage(close, length = 21)",' \
          '"ExpAverage(close, length = 34)","ExpAverage(close, length = 55)","ExpAverage(close, length = 88)",' \
          '"ExpAverage(close, length = 100)",BollingerBands().UpperBand,BollingerBands().LowerBand,CCI(),' \
-         'StochasticFull().FullD,StochasticFull().FullK,imp_volatility,volume,close,GetTime(),result_label'
+         'StochasticFull().FullD,StochasticFull().FullK,imp_volatility,volume,close,GetTime(),result_label,profit_loss'
 results.write(header + "\n")
 
-for row in range(sheet_obj.max_row-1):
+print(str(sheet_obj.max_row))
+
+for row in range(sheet_obj.max_row):
     if (row + 1) % 2 == 0:
+        print('row number ' + str(row + 1))
         if sheet_obj.cell(row = row + 1, column = 3).value.__contains__("Close"):
             print('FAIL at ' + str(row))
 
         cell_obj = sheet_obj.cell(row + 1, column=2)
         raw_data_list = cell_obj.value.split(" ")
-        print(raw_data_list)
+        # print(raw_data_list)
         raw_data_list = raw_data_list[1:]
 
         # formatting timestamp
@@ -33,26 +37,27 @@ for row in range(sheet_obj.max_row-1):
         raw_data_list[raw_data_list.__len__()-1] = str(round(formated_time, 2))
         raw_data_list = list(map(lambda x: x.replace('N/A', '0'), raw_data_list))
         raw_data_list = list(map(lambda x: x.replace(',', ''), raw_data_list))
-        print(raw_data_list)
+        # print(raw_data_list)
         s = ','.join(raw_data_list)
 
         profit_loss = sheet_obj.cell(row + 2, column=7).value
         is_loss = profit_loss.__contains__("(")
-        profit_loss = profit_loss.replace("(", "")
-        profit_loss = profit_loss.replace(")", "")
-        profit_loss = profit_loss.replace("$", "")
-        profit_loss = profit_loss.replace(",", "")
+        profit_loss = profit_loss.replace("(", "").replace(")", "").replace("$", "").replace(",", "")
 
         pl_size = float(profit_loss)/300
-        if pl_size >= 0.1 :
-            if is_loss :
-                s = s + "," + str('0')
-            else:
-                s = s + "," + str('1')
+        # if pl_size >= 0.1 :
+        if is_loss :
+            s = s + "," + str('0')
+        else:
+            s = s + "," + str('1')
         # else:
         #     s = s + "," + str('0')
 
-        results.write(s + "\n")
-        # print(s)
+        s = s + "," + (str('-') if is_loss else str('')) + profit_loss + "\n\n"
+
+        results.write(s)
+        print(s)
+
+
 
 
---
+++ src/ExcelReportFileConvertorForTradingView.py
@@ -0,0 +1,63 @@
+import openpyxl
+from datetime import datetime
+import time as time_lib
+
+
+path = r'C:\Users\i506998\OneDrive - SAP SE\Documents\private\Master Degree\Final Project\My research\TradingView\ERIK_TEST_MACD_Strategy_List_of_Trades_2022-08-08.xlsx'
+wb_obj = openpyxl.load_workbook(path)
+sheet_obj = wb_obj.active
+
+results = open("results_new_tradingview.csv", "a")
+results.truncate(0)
+
+header = 'MACD(),MACDdiff,MACD().Avg,RSI(),"ExpAverage(close, length = 9)","ExpAverage(close, length = 21)",' \
+         '"ExpAverage(close, length = 34)","ExpAverage(close, length = 55)","ExpAverage(close, length = 88)",' \
+         '"ExpAverage(close, length = 100)",BollingerBands().UpperBand,BollingerBands().LowerBand,CCI(),' \
+         'StochasticFull().FullD,StochasticFull().FullK,imp_volatility,volume,close,GetTime(),result_label,profit_loss'
+results.write(header + "\n")
+
+print(str(sheet_obj.max_row))
+
+for row in range(2, sheet_obj.max_row):
+    if (row) % 2 == 1:
+        print('row number ' + str(row))
+        if sheet_obj.cell(row = row, column = 2).value.__contains__("Exit"):
+            print('FAIL at ' + str(row))
+
+        cell_obj = sheet_obj.cell(row, column=3)
+        raw_data_list = cell_obj.value.split(" ")
+        # print(raw_data_list)
+        raw_data_list = raw_data_list[1:]
+
+        # formatting timestamp
+        raw_data_list[raw_data_list.__len__()-1] = raw_data_list[raw_data_list.__len__()-1].split(')')[0]
+        time = datetime.utcfromtimestamp(int(raw_data_list[raw_data_list.__len__()-1].replace(',', ''))/1000)
+        formated_time = time.hour + time.minute / 60
+
+        raw_data_list[raw_data_list.__len__()-1] = str(round(formated_time, 2))
+        raw_data_list = list(map(lambda x: x.replace('NaN', '0'), raw_data_list))
+        raw_data_list = list(map(lambda x: x.replace(',', ''), raw_data_list))
+        # print(raw_data_list)
+        s = ','.join(raw_data_list)
+
+        profit_loss = sheet_obj.cell(row, column=7).value
+        is_loss = True if profit_loss<0 else False
+        # profit_loss = profit_loss.replace("(", "").replace(")", "").replace("$", "").replace(",", "")
+
+        pl_size = float(profit_loss)/sheet_obj.cell(row, column=5).value
+        # if pl_size >= 0.1 :
+        if is_loss :
+            s = s + "," + str('0')
+        else:
+            s = s + "," + str('1')
+        # else:
+        #     s = s + "," + str('0')
+
+        s = s + "," + str(profit_loss) + "\n"
+
+        results.write(s)
+        print(s)
+
+
+
+
